8649dd55241ffe49f8ca24b7ac0cf33cfaea55ec
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-04-08 19:39:58 -0400

    updated api.js to call github api with username

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,5 +1,17 @@
-const api = {
-  getUser (username) {}
+const axios = require('axios')
+
+ const api = {
+  
+  getUser (username) {
+    return axios.get(`https://api.github.com/users/${username}`)
+       .then(function (res) {
+      //console.log(res.data)
+        return res.data
+       })
+  }
 }
 
 module.exports = api
+
+
+
