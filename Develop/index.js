43e528e944f60c4216b1f91245e241b158f80869
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-04-08 19:43:29 -0400

    updated index.js with questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,51 @@
-const questions = []
+const fs = require('fs')
+const inquirer = require('inquirer')
+const api = require('./utils/api.js')
+const markDown = require('./utils/generateMarkdown.js')
+
+
+
+
+const questions = [
+    {
+        type:"input",
+        name: "username",
+        message : "What is your Github username?"
+    },
+    {
+        type:"input",
+         name: "projecttitle",
+         message : "What is your project name?"
+
+    },
+    {
+        type:"input",
+         name: "description",
+         message : "What is your project descrption?"
+
+    },
+    {
+        type:"input",
+         name: "installation",
+         message : "What is the installation instruction required for the project?"
+
+    },
+    {
+        type:"list",
+         name: "LIcense",
+         message : "What type of licence required?",
+         choices : ['MIT','GPL 3.0','BSD 3.0']
+
+    },
+    {
+        type:"input",
+         name: "Test",
+         message : "What is the require command for testing?"
+
+    }
+
+];
+
 
 function writeToFile (fileName, data) {}
 
